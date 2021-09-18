61282d808aa60c0e305330ea59e1d2f85879ec74
SanskarKashyap <2005825@kiit.ac.in>
2021-09-18 21:56:13 +0530

    LLDelete

--- Abdul/DSA/LinkedList/193.cpp
+++ Abdul/DSA/LinkedList/193.cpp
@@ -6,15 +6,85 @@ struct node
     struct node *next;
 
 } * first;
-void create (int *A , int n )
+void create(int *A, int n)
 {
-    struct node *p,*t,*q;
-    first = new node ;
-    
+    struct node *t, *last;
+    first = new node;
+    first->data = A[0];
+    last = first;
+    for (int i = 1; i < n; i++)
+    {
+        t = new node;
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Display(node *dis)
+{
+    while (dis)
+    {
+        cout << dis->data << "\t";
+        dis = dis->next;
+    }
+    cout << "\n";
+}
+int delete_first()
+{
+    node *q;
+    q = first;
+    int x = first->data;
+    first = first->next;
+    delete q;
+    return x;
+}
+int count()
+{
+    node *coun;
+    coun = first;
+    int count = 0;
+    while (coun)
+    {
+        count++;
+        coun = coun->next;
+    }
+    return count;
+}
+void Delete_pos(int pos)
+{
+    node *p, *q;
+    p = new node;
+    q = new node;
+    p = first;
+    int position = 0;
+
+    if (pos > -1 && pos < count())
+    {
+
+        while (pos != position)
+        {
+            q = p;
+            p = p->next;
+            position++;
+        }
+        q->next = p->next;
+        delete p;
+    }
 }
 
 int main()
 {
-
+    int A[] = {2, 4, 7, 9, 12};
+    create(A, 5);
+    Display(first);
+    cout << count() << "\n";
+    delete_first();
+    Display(first);
+    cout << count() << "\n";
+    Delete_pos(2);
+    Display(first);
+    cout << count() << "\n";
     return 0;
 }
--- temp2.cpp
+++ temp2.cpp
@@ -1,64 +1,21 @@
 #include <iostream>
 using namespace std;
-struct stack
+class Node
 {
-    int top;
+public:
+    int data;
     int size;
-    int *S;
-};
-
-void create(stack *st)
-{
-
-    cin >> st->size;
-    st->S = new int(st->size);
-    st->top = -1;
-}
-void display(stack st)
-{
-    for (int i = st.size-1; i >= 0; i--)
+    Node *next;
+    Node(){};
+    Node(int data)
     {
-        cout << st.S[i] << " ";
+        this->data = data;
+        this->next = NULL;
     }
-    cout << endl;
-}
-void push(stack *st, int element)
+} top = NULL;
+void push(int element)
 {
-
-    if (st->top == st->size - 1)
-    {
-        cout << "Stack is full:( " << endl;
-    }
-    else
-    {
-        st->S[st->top] = element;
-        st->top++;
-    }
-}
-int pop(stack *st)
-{
-    int x = -1;
-    if (st->top == -1)
-    {
-        cout << "Stack is empty, there is no element to delete" << endl;
-    }
-    else
-    {
-        x = st->S[st->top];
-        st->top--;
-    }
-    return x;
-}
-int main()
-{
-    stack st;
-    create(&st);
-    push(&st, 10);
-    push(&st, 12);
-    push(&st, 15);
-    push(&st, 19);
-    display(st);
-
-
-    return 0;
+    Node *newNode;
+    newNode = new Node;
+    newNode->next
 }
